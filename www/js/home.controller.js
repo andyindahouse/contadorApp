179eb9a6af6f4afe810b370a772ee4f7dffa1e43
andy <anderson.mendez.contractor@bbva.com>
2015-09-16 16:11:05 +0200

    [feature] borrar contador

--- www/js/home.controller.js
+++ www/js/home.controller.js
@@ -2,8 +2,10 @@
 
 angular.module('contadorApp')
 
-.controller('HomeCtrl', ['$scope','$state', 
-	function ($scope, $state){  
+.controller('HomeCtrl', ['$scope','$state', '$window', '$ionicPopup',
+	function ($scope, $state, $window, $ionicPopup){  
+
+		var self=this;
 
 		$scope.amount = window.localStorage.getItem('amount');
 
@@ -13,8 +15,12 @@ angular.module('contadorApp')
 						{name:'Estornudos',amount:69,selected:true}]
 
 		$scope.selected = {name: '', amount: 100}
+
+		$scope.deleteOn = false;
+		$scope.label = 'Borrar';
 		
 		$scope.navigateTo = function (state) {
+
 			$state.go(state);
 		};
 
@@ -22,32 +28,75 @@ angular.module('contadorApp')
 			
 			console.log("Incremento contador...");	
 			$scope.amount = parseInt($scope.amount) + 1;
-			window.localStorage.setItem('amount', $scope.amount);
-			
+			$window.localStorage.setItem('amount', $scope.amount);		
 		};
 
 		$scope.toRestartCount = function () {
 			console.log("Reinicio contador...");
 			$scope.amount = 0;			
-			window.localStorage.setItem('amount', 0);
+			$window.localStorage.setItem('amount', 0);
 		};
+
+
+		$scope.showConfirm = function() {
+   			var confirmPopup = $ionicPopup.confirm({
+     								title: 'Borrar contador',
+     								template: '¿Estás seguro que deseas eliminar el contador?'});
+   			confirmPopup
+   			.then(function(res) {
+     			if(res) {
+       				console.log('You are sure');
+     			} else {
+       				console.log('You are not sure');
+     			}
+   			});
+ };
+
 
 		$scope.toChangeSelected = function (selected) {
-			var item = {};
-			for (var i = 0; i<$scope.items.length; i++) {
-				item = $scope.items[i];	
-				if(item.name === selected.name){
-					item.selected = true;
-				}else{
-					item.selected = false;	
-				}							
-			};
 
-			selected.selected=true;
+			if($scope.deleteOn){
+
+				
+			}else{
+				var item = {};
+				for (var i = 0; i<$scope.items.length; i++) {
+					item = $scope.items[i];	
+					if(item.name === selected.name){
+						item.selected = true;
+					}else{
+						item.selected = false;	
+					}							
+				};
+
+				selected.selected=true;	
+			}			
 		};
 
-		var _toCreateCount = function (name) {
+		var _deleteItem = function (item) {
 			
+		};
+
+		var _selectItem = function (item) {
+			
+		};
+
+		$scope.toChangeMod = function() {
+
+			if($scope.deleteOn){
+				$scope.deleteOn = false;
+				$scope.label = 'Borrar';
+			} else{
+				$scope.label = 'Cancelar';
+				$scope.deleteOn	= true;				
+			}
+		};								
+
+		$scope.toDeleteSelected = function (selected) {
+
+			
+
+
 			/*var selected = '';
 			if($window.localStorage.getItem('selected')){
 				selected = $window.localStorage.getItem('selected');
